9f8044d7f8c002f43515095b187f2edba8001794
Ramon Geronimo <rgero215@gmail.com>
2019-08-24 21:32:23 -0700

    uncomment urlspatterns

--- API100React/urls.py
+++ API100React/urls.py
@@ -40,5 +40,5 @@ urlpatterns = [
 ]
 
 urlpatterns += staticfiles_urlpatterns()
-# if settings.DEBUG:
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
